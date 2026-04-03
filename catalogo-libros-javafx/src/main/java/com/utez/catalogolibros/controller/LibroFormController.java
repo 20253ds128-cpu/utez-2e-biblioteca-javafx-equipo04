34bb000ab375dd3785978f9102ad0b702d82d5d9
Luis Fernando Camacho <20253ds128@utez.edu.mx>
2026-04-02 20:28:55 -0600

    feat: Implementación de guardado de libros en archivo CSV desde formulario

--- catalogo-libros-javafx/src/main/java/com/utez/catalogolibros/controller/LibroFormController.java
+++ catalogo-libros-javafx/src/main/java/com/utez/catalogolibros/controller/LibroFormController.java
@@ -1,5 +1,8 @@
 package com.utez.catalogolibros.controller;
 
+import com.utez.catalogolibros.model.Libro;
+import com.utez.catalogolibros.repository.LibroRepository;
+import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -29,4 +32,31 @@ public class LibroFormController {
     @FXML
     private Button btnCancelar;
 
+    private final LibroRepository repository = new LibroRepository();
+
+    @FXML
+    private void onGuardar() {
+        try {
+            String isbn = txtIsbn.getText();
+            String titulo = txtTitulo.getText();
+            String autor = txtAutor.getText();
+            int anio = Integer.parseInt(txtAnio.getText());
+            String genero = txtGenero.getText();
+            boolean disponible = chkDisponible.isSelected();
+
+            Libro libro = new Libro(isbn, titulo, autor, anio, genero, disponible);
+
+            var lista = repository.cargarLibros();
+
+            lista.add(libro);
+
+            repository.guardarLibros(lista);
+
+            Stage stage = (Stage) btnGuardar.getScene().getWindow();
+            stage.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
