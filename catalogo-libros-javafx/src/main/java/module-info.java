0e9a92921b134b036f68a5ea74a968161457bdb9
Emiliano_2E <20253ds125@utez.edu.mx>
2026-04-08 09:15:50 -0600

    Nueva version de git

---
+++ catalogo-libros-javafx/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.biblioteca.catalogolibrosjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.biblioteca.catalogolibrosjavafx to javafx.fxml;
+    exports com.biblioteca.catalogolibrosjavafx;
+}
