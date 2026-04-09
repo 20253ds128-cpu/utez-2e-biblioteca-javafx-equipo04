364eda1987c7eb89bb047ed4e831b1b696af9091
Emiliano_2E <20253ds125@utez.edu.mx>
2026-04-09 01:42:45 -0600

    separador

--- catalogo-libros-javafx/src/main/java/com/biblioteca/services/LibroServices.java
+++ catalogo-libros-javafx/src/main/java/com/biblioteca/services/LibroServices.java
@@ -2,6 +2,10 @@ package com.biblioteca.services;
 
 import com.biblioteca.model.Libro;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -54,5 +58,59 @@ public class LibroServices {
 
     }
 
+public void eliminar (String isbn){
+        boolean eliminado = catalogo.removeIf(lib-> lib.getIsbn().equalsIgnoreCase(isbn));
+        if (!eliminado){
+            throw new IllegalArgumentException("No se encontro el libro con ISBN:" + isbn);
 
+        }
+        guardarEnArchivo();
+}
+
+public void cargarDesdeArchivo(){
+        catalogo.clear();
+        File archivo = new File (ARCHIVO_DATOS);
+
+        if (!archivo.exists()){
+            System.out.println("[LibroServices] Archivo de datos no encontrado. Se iniciara con catalogo vacio");
+            return;
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(archivo))){
+            String linea;
+            int numeroLinea = 0;
+
+            while ((linea = br.readLine()) != null){
+                numeroLinea++;
+                linea = linea.trim();
+                if (linea.isEmpty() || linea.startsWith("#")) continue;
+
+                String[] partes = linea.split(SEPARADOR, -1);
+                if (partes.length != 6){
+                    System.out.printf("Linea " + numeroLinea + "mal formada , se omite" + linea);
+                    continue;
+                }
+                try {
+                    String isbn      = partes[0].trim();
+                    String titulo    = partes[1].trim();
+                    String autor     = partes[2].trim();
+                    int    anio      = Integer.parseInt(partes[3].trim());
+                    String genero    = partes[4].trim();
+                    boolean disp     = partes[5].trim().equalsIgnoreCase("true");
+
+                    catalogo.add(new Libro(isbn, titulo, autor, anio, genero, disp));
+                } catch (NumberFormatException e) {
+                    System.err.println("[LibroService] Error de formato en línea " + numeroLinea + ": " + e.getMessage());
+                }
+            }
+            System.out.println("[LibroService] Catálogo cargado: " + catalogo.size() + " libro(s).");
+
+        } catch (IOException e) {
+            System.err.println("[LibroService] Error al leer el archivo: " + e.getMessage());
+        }
+}
+
+            }
+
+}
 }
